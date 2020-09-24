88d0946c6b9cb53b3cf55b4c55fc9d48616ccba3
limin100 <limin100>
2020-09-24 11:45:02 +0800

    auto save seconds

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -583,7 +583,7 @@ If no region is selected, `kill-ring' or clipboard is used instead."
 ;; {{ auto-save.el
 (local-require 'auto-save)
 (add-to-list 'auto-save-exclude 'file-too-big-p t)
-(setq auto-save-idle 2) ; 2 seconds
+(setq auto-save-idle 300) ; 2 seconds
 (auto-save-enable)
 (setq auto-save-slient t)
 ;; }}
