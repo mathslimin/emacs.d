736123dacc2fb5a1bbbc7371ae7b1ae473a8113d
mathslimin <mathslimin@gmail.com>
2021-04-06 11:37:20 +0800

    Update custom.el

--- custom.el
+++ custom.el
@@ -46,11 +46,11 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 ;prettier-js
-(add-to-list 'load-path "~/.emacs.d/projs/myelpa/prettier-js-20180109.726")
-(require 'prettier-js)
-(add-hook 'js2-mode-hook 'prettier-js-mode)
-(add-hook 'web-mode-hook 'prettier-js-mode)
-(add-hook 'typescript-mode-hook 'prettier-js-mode)
+;(add-to-list 'load-path "~/.emacs.d/projs/myelpa/prettier-js-20180109.726")
+;(require 'prettier-js)
+;(add-hook 'js2-mode-hook 'prettier-js-mode)
+;(add-hook 'web-mode-hook 'prettier-js-mode)
+;(add-hook 'typescript-mode-hook 'prettier-js-mode)
 
 ;indent setting
 (setq-default tab-width 4)
@@ -148,3 +148,32 @@ lsp-on-touch-time) 120) ;; 2 mins
   (format-all-mode)
 ))
 
+
+;; 编码设置 begin
+(set-language-environment 'Chinese-GB)
+
+;; default-buffer-file-coding-system变量在emacs23.2之后已被废弃，使用buffer-file-coding-system代替
+(set-default buffer-file-coding-system 'utf-8-unix)
+(set-default-coding-systems 'utf-8-unix)
+(setq-default pathname-coding-system 'euc-cn)
+(setq file-name-coding-system 'euc-cn)
+
+;; 另外建议按下面的先后顺序来设置中文编码识别方式。
+;; 重要提示:写在最后一行的，实际上最优先使用; 最前面一行，反而放到最后才识别。
+;; utf-16le-with-signature 相当于 Windows 下的 Unicode 编码，这里也可写成
+;; utf-16 (utf-16 实际上还细分为 utf-16le, utf-16be, utf-16le-with-signature等多种)
+(prefer-coding-system 'cp950)
+(prefer-coding-system 'gb2312)
+(prefer-coding-system 'cp936)
+(prefer-coding-system 'gb18030)
+
+;(prefer-coding-system 'utf-16le-with-signature)
+(prefer-coding-system 'utf-16)
+
+;; 新建文件使用utf-8-unix方式
+;; 如果不写下面两句，只写
+;; (prefer-coding-system 'utf-8)
+;; 这一句的话，新建文件以utf-8编码，行末结束符平台相关
+(prefer-coding-system 'utf-8-dos)
+(prefer-coding-system 'utf-8-unix)
+;; 编码设置 end
